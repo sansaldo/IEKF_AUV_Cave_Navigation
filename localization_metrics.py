5e90bc54d3a861d55e0cec09465200ac8a19055f
Shane Storks <shanestorks@gmail.com>
2021-04-10 13:26:48 -0400

    Add cone metrics

--- localization_metrics.py
+++ localization_metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
+from itertools import product
 
 # Calculates MSE of ground truth (gt) and predicted trajectories
 # gt and predicted should be shape (number of timesteps, number of dimensions),
@@ -36,3 +37,71 @@ def mse(gt, predicted, gt_times=None, predicted_times=None):
         mse = mean_squared_error(gt_new.flatten(), predicted_new.flatten())
 
     return mse
+
+# Measures 2 cone metrics from Angelos et al., 2016
+# pos: array of shape (number of pred timesteps, 3) for xyz predictions to evaluate
+# times: array of shape (number of pred timesteps) for the evaluated estimation
+# cone_times: array of shape (number of cones, 2) for the two times each cone was seen
+# cone_distances: known distances between cones; array of shape (number of cones, number of cones) where [i,j] = distance between cones i and j
+# cone_offsets: (optional) array of shape (number of cones, 2, 3) containing the xyz offset for each cone based on first and second camera observation
+# Returns a dictionary of metrics
+def cone_metrics(pos, times, cone_times, cone_distances, cone_offsets=None):
+    return_metrics = {}
+    no_cones = cone_times.shape[0]
+    for i in range(no_cones):
+        cone_time0 = cone_times[i, 0]
+        cone_time1 = cone_times[i, 1]
+        
+        pred_xyz0 = pos[np.argmin(np.abs(times - cone_time0)), :]
+        pred_xyz1 = pos[np.argmin(np.abs(times - cone_time1)), :]
+
+        if cone_offsets is not None:
+            pred_xyz0 += cone_offsets[i,0,:]
+            pred_xyz1 += cone_offsets[i,1,:]
+
+        return_metrics['%s_2pass_abs_error' % str(i)] = np.abs(pred_xyz0 - pred_xyz1)
+        return_metrics['%s_2pass_error^2' % str(i)] = (pred_xyz0 - pred_xyz1) ** 2
+        return_metrics['%s_2pass_2norm' % str(i)] = np.linalg.norm(pred_xyz0 - pred_xyz1, ord=2)
+
+    for i, j in product(list(range(no_cones)), repeat=2):
+        if i == j:
+            continue
+
+        # First cone (first and second pass)
+        cone0_time0 = cone_times[i, 0]
+        cone0_time1 = cone_times[i, 1]
+        pred0_xyz0 = pos[np.argmin(times - cone0_time0), :]
+        pred0_xyz1 = pos[np.argmin(times - cone0_time1), :]
+
+        # Second cone (first and second pass)
+        cone1_time0 = cone_times[j, 0]
+        cone1_time1 = cone_times[j, 1]
+        pred1_xyz0 = pos[np.argmin(np.abs(times - cone1_time0)), :]
+        pred1_xyz1 = pos[np.argmin(np.abs(times - cone1_time1)), :]
+
+        if cone_offsets is not None:
+            pred0_xyz0 += cone_offsets[i,0,:]
+            pred0_xyz1 += cone_offsets[i,1,:]
+
+            pred1_xyz0 += cone_offsets[j,0,:]
+            pred1_xyz1 += cone_offsets[j,1,:]            
+
+        # Distance of first pass between these cones, and second pass between these cones
+        dist0 = np.linalg.norm(pred0_xyz0 - pred1_xyz0)
+        dist1 = np.linalg.norm(pred0_xyz1 - pred1_xyz1)
+
+        gt_cone_distance = cone_distances[i,j]
+        error0 = dist0 - gt_cone_distance
+        error1 = dist1 - gt_cone_distance
+
+        # Add each pass' error and the average pass error
+        return_metrics['%s_%s_pass0_error' % (str(i), str(j))] = error0
+        return_metrics['%s_%s_pass1_error' % (str(i), str(j))] = error1
+        return_metrics['%s_%s_avg_error' % (str(i), str(j))] = (error0 + error1) / 2.
+
+    return return_metrics
+        
+
+
+    
+
