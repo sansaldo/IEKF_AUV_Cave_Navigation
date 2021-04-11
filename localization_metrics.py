15df9dbb245d7cfabc0d54fea3cbe4ca172607c5
Shane Storks <shanestorks@gmail.com>
2021-04-11 15:56:29 -0400

    Fixed second cone metric for distances between cones

--- localization_metrics.py
+++ localization_metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from itertools import product
-from constants import cone_times, cone_distances
+from constants import cone_times, cone_times_ordered, cone_distances, cone_path
 
 # Calculates MSE of ground truth (gt) and predicted trajectories
 # gt and predicted should be shape (number of timesteps, number of dimensions),
@@ -60,42 +60,44 @@ def cone_metrics(pos, times, cone_offsets=None):
         return_metrics['%s_2pass_error^2' % str(i)] = (pred_xyz0 - pred_xyz1) ** 2
         return_metrics['%s_2pass_2norm' % str(i)] = np.linalg.norm(pred_xyz0 - pred_xyz1, ord=2)
 
-    for i, j in product(list(range(no_cones)), repeat=2):
-        # We don't have all distances - if we don't, don't compute metric
-        if cone_distances[i,j] <= 0.:
+   
+    for step, i in enumerate(cone_path[:-1]):
+        j = cone_path[step+1]
+
+        # We don't have all distances along the path
+        if (i, j) not in cone_distances:
             continue
 
-        # First cone (first and second pass)
-        cone0_time0 = cone_times[i, 0]
-        cone0_time1 = cone_times[i, 1]
-        pred0_xyz0 = pos[np.argmin(times - cone0_time0), :]
-        pred0_xyz1 = pos[np.argmin(times - cone0_time1), :]
+        # Initial cone time and position
+        cone0_time = cone_times_ordered[step]
+        cone0_idx = np.argmin(np.abs(times - cone0_time))
+        pred0_xyz = pos[cone0_idx, :]
 
-        # Second cone (first and second pass)
-        cone1_time0 = cone_times[j, 0]
-        cone1_time1 = cone_times[j, 1]
-        pred1_xyz0 = pos[np.argmin(np.abs(times - cone1_time0)), :]
-        pred1_xyz1 = pos[np.argmin(np.abs(times - cone1_time1)), :]
+        # Terminal cone time and position
+        cone1_time = cone_times_ordered[step + 1]
+        cone1_idx = np.argmin(np.abs(times - cone1_time))
+        pred1_xyz = pos[cone1_idx, :]
 
-        if cone_offsets is not None:
-            pred0_xyz0 += cone_offsets[i,0,:]
-            pred0_xyz1 += cone_offsets[i,1,:]
+        print('Cones',i,j)
+        print(cone0_time, '->', cone1_time)
+        print(cone0_idx, '->', cone1_idx)
+        print(pred0_xyz.shape)
 
-            pred1_xyz0 += cone_offsets[j,0,:]
-            pred1_xyz1 += cone_offsets[j,1,:]            
+        # Sanity check
+        assert cone0_idx < cone1_idx
 
-        # Distance of first pass between these cones, and second pass between these cones
-        dist0 = np.linalg.norm(pred0_xyz0 - pred1_xyz0)
-        dist1 = np.linalg.norm(pred0_xyz1 - pred1_xyz1)
+        # NOTE: cone offsets not incorporated here (probably won't be)
 
-        gt_cone_distance = cone_distances[i,j]
-        error0 = dist0 - gt_cone_distance
-        error1 = dist1 - gt_cone_distance
+        # Calculate traveled distance between cones
+        pred_dist = 0.0
+        for k in range(cone0_idx, cone1_idx):
+            pred_dist += np.linalg.norm(pos[k+1] - pos[k])
 
-        # Add each pass' error and the average pass error
-        return_metrics['%s_%s_pass0_error' % (str(i), str(j))] = error0
-        return_metrics['%s_%s_pass1_error' % (str(i), str(j))] = error1
-        return_metrics['%s_%s_avg_error' % (str(i), str(j))] = (error0 + error1) / 2.
+        gt_cone_distance = cone_distances[(i,j)]
+        error = pred_dist - gt_cone_distance
+
+        # Add the error for this segment of the path
+        return_metrics['%s_%s_dist_error' % (str(i), str(j))] = error
 
     return return_metrics
         
